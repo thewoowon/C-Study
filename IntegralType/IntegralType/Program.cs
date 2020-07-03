6e1195e2dc02b4bef4211211aef2a5ae70a22db2
이하영 <gkdudp9@naver.com>
2020-07-03 14:31:40 +0900

    CSharpStart

--- IntegralType/IntegralType/Program.cs
+++ IntegralType/IntegralType/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloApp
+namespace IntegralType
 {
     class Program
     {
         static void Main(string[] args)
         {
+            sbyte a = -10;
+            byte b = 40;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+
         }
     }
 }
---
+++ StudyCSharp/HelloApp/HelloApp.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+/// <summary>
+///  Hello Guys! i've wanted to talk about our team's main event project!. we check the project everyday that as well as we can do 
+/// </summary>
+
+
+namespace HelloApp
+{
+    class HelloApp
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                WriteLine("사용법 : helloapp.exe <이름>");
+                return;
+            }
+            WriteLine($"Hello, {args[0]}!");
+        }
+    }
+}
